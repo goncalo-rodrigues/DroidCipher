8d1bd8ead4f383458908c68327935bc3752ba809
Nuno Afonso <nuno.c.afonso@ist.utl.pt>
2016-11-10 16:38:35 +0000

    Added bluetooth client, to be tested

--- Android/app/src/main/java/pt/ulisboa/tecnico/sirs/droidcipher/ServerThread.java
+++ Android/app/src/main/java/pt/ulisboa/tecnico/sirs/droidcipher/ServerThread.java
@@ -10,23 +10,21 @@ import java.util.UUID;
 
 public class ServerThread extends Thread {
     private final BluetoothServerSocket mmServerSocket;
+    private final Context context;
 
-    private static final String NAME = "DroidCipher"; //TODO: Use the one from strings.xml
-    private static final UUID MY_UUID =
-            UUID.fromString("d1418830-a213-11e6-bdf4-0800200c9a66");
-
-
-    public ServerThread() {
+    public ServerThread(Context context) {
         // Use a temporary object that is later assigned to mmServerSocket,
         // because mmServerSocket is final
         BluetoothServerSocket tmp = null;
         InputStream tmpIn = null;
+        this.context = context;
 
         BluetoothAdapter device = BluetoothAdapter.getDefaultAdapter();
 
         try {
             // MY_UUID is the app's UUID string, also used by the client code
-            tmp = device.listenUsingRfcommWithServiceRecord(NAME, MY_UUID);
+            tmp = device.listenUsingRfcommWithServiceRecord(context.getString(R.string.app_name),
+                    UUID.fromString(context.getString(R.string.UUID)));
         } catch (IOException e) { }
 
         mmServerSocket = tmp;
