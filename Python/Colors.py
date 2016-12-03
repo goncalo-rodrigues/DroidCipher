91286cdc4e5299e8e18eb44118224c5649e847c5
DiogoMRSilva <dyx_silva@hotmail.com>
2016-12-03 10:33:06 +0000

    cheking file key integrity

---
+++ Python/Colors.py
@@ -0,0 +1,21 @@
+class colors:
+    BLACK   =   '\033[30m'
+    RED     =   '\033[31m'
+    BRED    = '\x1b[31;4m'
+    BREDRESET = '\x1b[5;39;49m'
+
+    GREEN   =   '\033[32m'
+    BGREEN  = '\x1b[1;30;42m'
+
+    YELLOW  =   '\033[33m'
+    BYELLOW =   '\033[43m'
+
+    BLUE    =   '\033[34m'
+    BYELLOWRED = '\x1b[1;31;43m'
+
+    BLUE2   =   '\033[36m'
+
+    RESET   =   '\033[39m'
+    BRESET = '\x1b[5;39;49m'
+
+    BLINK = '\033[4m'
--- Python/FileOperations.py
+++ Python/FileOperations.py
@@ -6,6 +6,7 @@ import base64
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
+from Colors import colors
 
 
 
@@ -26,9 +27,12 @@ def encrypt_file(keySize, filename, path):
     print("created file key:" + base64.b64encode(key))
     public_key = RSA.importKey(open(path +'cert/public_key.txt').read(), passphrase='password')
     asymmetric_cipher = PKCS1_OAEP.new(public_key, hashAlgo = SHA256)#mudar isto para sha256
+    h = SHA256.new()
+    h.update(key)
+    hashedkey = h.digest()
     cipher_key = asymmetric_cipher.encrypt(key)
     iv = Random.new().read(AES.block_size)
-    marshal.dump([file_size, cipher_key, iv], metadata_file)
+    marshal.dump([file_size, cipher_key, iv, hashedkey], metadata_file)
 
     cipher = AES.new(key, AES.MODE_CBC, iv)
 
@@ -67,8 +71,26 @@ def decrypt_file(filename, path, socket):
     """saves info to later decipher"""
     file_size = metadata[0]
     encrypted_key = metadata[1]
-    key = socket.decrypt_key(encrypted_key)
     iv = metadata[2]
+    hashedkey = metadata[3]
+
+    key = socket.decrypt_key(encrypted_key)
+    if key == None:
+        input_file.close()
+        output_file.close()
+        metadata_file.close()
+        print(colors.RED + "X----> Some problem in geting the key from android !!!" + colors.RESET)
+        return
+
+    h = SHA256.new()
+    h.update(key)
+    newhashedkey = h.digest()
+    if newhashedkey != hashedkey:
+        input_file.close()
+        output_file.close()
+        metadata_file.close()
+        print(colors.RED+"X----> the key received is not equal to the one created !!!"+colors.RESET)
+        return
 
     decipher = AES.new(key, AES.MODE_CBC, iv)
 
